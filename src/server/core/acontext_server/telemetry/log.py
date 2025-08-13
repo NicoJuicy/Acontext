a6b5ef370869f3c8adcf1b4810793a461ef541f6
Gus <jianbaiye@outlook.com>
2025-08-13 21:05:17 +0800

    feat: add json logger

--- src/server/core/acontext_server/telemetry/log.py
+++ src/server/core/acontext_server/telemetry/log.py
@@ -1,23 +1,68 @@
+import os
+import sys
 import logging
+import structlog
 from ..util.terminal_color import TerminalColorMarks
 
-LOG = None
+
+def __get_json_logger():
+    shared_processors = [
+        structlog.contextvars.merge_contextvars,
+        structlog.stdlib.add_logger_name,
+        structlog.stdlib.add_log_level,
+        structlog.stdlib.PositionalArgumentsFormatter(),
+        structlog.stdlib.ExtraAdder(),
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.processors.CallsiteParameterAdder(
+            [
+                structlog.processors.CallsiteParameter.LINENO,
+                structlog.processors.CallsiteParameter.PATHNAME,
+            ]
+        ),
+    ]
+
+    structlog_processors = shared_processors + [
+        structlog.processors.dict_tracebacks,
+        structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+    ]
+
+    structlog.configure(
+        processors=structlog_processors,
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        cache_logger_on_first_use=True,
+    )
+
+    formatter = structlog.stdlib.ProcessorFormatter(
+        foreign_pre_chain=shared_processors,
+        processors=[
+            structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+            structlog.processors.JSONRenderer(),
+        ],
+    )
+
+    handler = logging.StreamHandler(stream=sys.stdout)
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger("acontext-core")
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    return logger
 
 
-def get_global_logger(level: int = logging.INFO):
-    global LOG
-    if LOG is not None:
-        return LOG
+def __get_text_logger():
     formatter = logging.Formatter(
-        f"{TerminalColorMarks.BOLD}{TerminalColorMarks.BLUE}%(name)s |{TerminalColorMarks.END}  %(levelname)s - %(asctime)s  -  %(message)s"
+        f"{TerminalColorMarks.BOLD}{TerminalColorMarks.BLUE}%(name)s |{TerminalColorMarks.END} %(asctime)s - %(levelname)s - %(message)s"
     )
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
-    logger = logging.getLogger("acontext")
-    logger.setLevel(level)
+    logger = logging.getLogger("acontext-core")
+    logger.setLevel(logging.INFO)
     logger.addHandler(handler)
-    LOG = logger
-    return LOG
+    return logger
 
 
-get_global_logger()
+LOG_FORMAT = os.getenv("LOG_FORMAT", "text")
+if LOG_FORMAT == "json":
+    LOG = __get_json_logger()
+else:
+    LOG = __get_text_logger()
